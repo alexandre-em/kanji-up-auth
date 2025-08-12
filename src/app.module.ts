af0a87cc4d3442e13a8c8d7bf5d25c26ff9eb2dc
Alexandre Em <a.em@qivia.fr>
2025-08-12 18:19:21 +0200

    chore: add health endpoint to check server health

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { HealthController } from './api/v1/health/health.controller';
 import { UsersModule } from './api/v1/users/users.module';
 
 @Module({
@@ -10,7 +11,7 @@ import { UsersModule } from './api/v1/users/users.module';
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(`${process.env.DATABASE_URI}/usersv2?retryWrites=true&w=majority`),
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
