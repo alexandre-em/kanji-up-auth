e445dd17eeb795d2b94d771611f56d22872defe7
Alexandre Em <a.em@qivia.fr>
2025-08-12 17:53:37 +0200

    chore: add and init mongoose + env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(`${process.env.DATABASE_URI}/usersv2?retryWrites=true&w=majority`),
   ],
   controllers: [],
   providers: [],
